b3552e87db5536ce1b0a34be77f63d7a9bc50ac2
Lokeshg27 <Lokeshg27@users.noreply.github.com>
2023-08-07 20:23:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const _input = document.createElement('input');
+_input.type = 'range';
+_input.max = 10;
+_input.value = 0;
+document.body.appendChild(_input);
+const div = document.createElement('div');
+document.body.appendChild(div);
+
+_input.oninput = addStars;
+
+function addStars() {
+    div.innerText = '';
+    for (let i = 0; i < _input.value; i++) {
+        div.append('0');
+    }
+}
